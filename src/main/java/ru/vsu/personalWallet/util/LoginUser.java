2f3f525f6dc520d726842d5d48d32350a6042e16
Valentina Endovitskaya <valyalark@hotmail.com>
2018-03-18 17:39:26 +0300

    Data annotation bug fixed

--- src/main/java/ru/vsu/personalWallet/util/LoginUser.java
+++ src/main/java/ru/vsu/personalWallet/util/LoginUser.java
@@ -1,11 +1,32 @@
 package ru.vsu.personalWallet.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-@Data
-@AllArgsConstructor
+@EqualsAndHashCode
 public class LoginUser {
     private String email;
     private String password;
+
+    public LoginUser(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LoginUser setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    LoginUser setPassword(String password) {
+        this.password = password;
+        return this;
+    }
 }
